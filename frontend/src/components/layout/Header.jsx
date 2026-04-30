ac37276a9303ff668fba3eaab83390ecdd5d2186
Masood Buriro <masoodaliburiro999@gmail.com>
2026-05-01 00:37:44 +0500

    Changes in Header

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -146,9 +146,9 @@ const Header = () => {
 
           {/* Desktop Navigation */}
           <nav className="hidden lg:flex items-center space-x-1">
-            <Link to="/case-studies" className={`px-4 py-2 rounded-xl font-medium transition-all duration-300 ${textColor} hover:bg-white/10 hover:scale-105`}>
+            {/* <Link to="/case-studies" className={`px-4 py-2 rounded-xl font-medium transition-all duration-300 ${textColor} hover:bg-white/10 hover:scale-105`}>
               Work
-            </Link>
+            </Link> */}
             
             {/* Services Dropdown - FULL WIDTH AT DESKTOP */}
             <div
